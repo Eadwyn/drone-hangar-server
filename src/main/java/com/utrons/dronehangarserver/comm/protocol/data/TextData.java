675812240a558654a175080c23c68cfe39c79ddf
Eadwyn <EadwynZhu@gmail.com>
2020-05-13 22:06:19 +0800

    修改页面

--- src/main/java/com/utrons/dronehangarserver/comm/protocol/data/TextData.java
+++ src/main/java/com/utrons/dronehangarserver/comm/protocol/data/TextData.java
@@ -1,11 +1,13 @@
 package com.utrons.dronehangarserver.comm.protocol.data;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.utrons.dronehangarserver.comm.protocol.request.ProtocolRequest;
 import com.utrons.dronehangarserver.util.NumericUtil;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 
 @Data
 public class TextData implements Serializable {
@@ -16,6 +18,10 @@ public class TextData implements Serializable {
 	/** 【S8】在界面文本框内显示 */
 	private String text;
 
+	/** 接收到的时间 */
+	@JsonFormat(pattern = "HH:mm:ss SSS", timezone = "GWT+8")
+	private Date time;
+
 	public static TextData parse(ProtocolRequest request) {
 		byte[] data = request.getPkgPayload();
 
@@ -25,10 +31,11 @@ public class TextData implements Serializable {
 		byte[] text = new byte[request.getPkgLength() - 22];
 		System.arraycopy(data, 19, text, 0, text.length);
 		try {
-			obj.setText(new String(text, "GBK"));
+			obj.setText(new String(new String(text, "gb2312").getBytes("utf-8")));
 		} catch (UnsupportedEncodingException e) {
 			// ignore
 		}
+		obj.setTime(new Date());
 		return obj;
 	}
 }
